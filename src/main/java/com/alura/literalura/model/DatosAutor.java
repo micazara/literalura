e37098f15cb61ca42f6bef343c1b2001b0cde651
Mica Zara <zara_micaela@outlook.com>
2024-11-26 11:44:16 -0300

    entidades para la deserializacion

---
+++ src/main/java/com/alura/literalura/model/DatosAutor.java
@@ -0,0 +1,13 @@
+package com.alura.literalura.model;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosAutor(
+        @JsonAlias("name") String nombre,
+        @JsonAlias("birth_year") String fechaNacimiento,
+        @JsonAlias("death_year") String fechaFallecimiento) {
+}
---
+++ src/main/java/com/alura/literalura/model/DatosLibro.java
@@ -0,0 +1,19 @@
+package com.alura.literalura.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosLibro(
+                @JsonAlias("title") String titulo,
+                @JsonAlias("authors") List<DatosAutor> autores,
+                @JsonAlias("languages") List<String> idiomas,
+                @JsonAlias("download_count") Integer cantidadDescargas) {
+
+        public String obtenerPrimerIdioma() {
+                return (idiomas != null && !idiomas.isEmpty()) ? idiomas.get(0) : null;
+        }
+
+}
